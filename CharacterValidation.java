9087fcddf0340279ffaa70d67e7cfb2d445299cc
AadnyaKulkarni <aadnya.kulkarni@cumminscollege.in>
2026-02-10 09:47:03 +0530

    Char Vali file created

---
+++ CharacterValidation.java
@@ -0,0 +1,45 @@
+
+public class CharacterValidation
+{
+    boolean upperCase=false;  
+    boolean lowerCase=false;
+
+    public boolean hasUpper(String input){
+        int i;
+        for(i=0; i<input.length(); i++)
+        {
+            if(Character.isUpperCase(input.charAt(i)))
+            {
+                upperCase=true;
+            }
+        }
+
+        return upperCase;
+    }
+    
+    public boolean hasLower(String input){
+
+        int i;
+        for(i=0; i<input.length(); i++)
+        {
+            if(Character.isLowerCase(input.charAt(i)))
+            {
+                lowerCase=true;
+            }
+
+        }
+        
+        return lowerCase;
+    }
+
+    public boolean hasUpperLower(String input)
+    {
+        boolean uC = hasUpper(input);
+        boolean lC = hasLower(input);
+        if(uC==true && lC==true)
+        {
+            return true;
+        }
+        return false;
+    }
+}
